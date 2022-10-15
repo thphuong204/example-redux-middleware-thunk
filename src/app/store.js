2077ab33b5711d0d0b189e0519a0cf5c22b9ddea
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-15 16:00:35 +0700

    store @multiCounterReducer in @store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,14 @@
 import { createStore,combineReducers } from "redux";
 import counterReducer from "../features/counter/reducer";
 import {composeWithDevTools} from "redux-devtools-extension";
+import multiCounterReducer from "../features/multiCounter/reducer";
 
 
 const initialState = {}; //global state
 const store = createStore(
     combineReducers({
-        counter: counterReducer
+        counter: counterReducer,
+        multiCounter: multiCounterReducer,
     }),
     initialState,
     composeWithDevTools()
