d1b1ea99d4167c35fabf3a0f58086b67c9efac90
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-16 16:15:12 +0700

    create todoReducer

---
+++ src/features/todo/reducer.js
@@ -0,0 +1,41 @@
+export const ADD_TODO = "TODO.ADD";
+export const TOGGLE_TODO = "TODO.TOGGLE";
+export const SET_FILTER = "TODO.SET_FILTER";
+
+const initialState={
+    todos: [],
+    filter:"SHOW_ALL",
+}
+
+const todoReducer = (state=initialState,action) => {
+    const {type,payload} = action;
+    switch (type) {
+        case ADD_TODO:
+            return {
+                ...state, 
+                todos:[
+                    ...state.todos,
+                    {
+                        id: payload.id, 
+                        text: payload.text,
+                        completed: false
+                    }
+                ]
+            };
+        case TOGGLE_TODO:
+            return {
+                ...state,
+                // lưu ý chỗ todos ngay bên dưới, không dùng dấu ngoặc array bao quanh state.todos.map
+            todos: state.todos.map((todo)=> {
+                    if (todo.id !== payload.id) return todo 
+                    return {...todo, completed: !todo.completed};
+                }),
+        };
+        case SET_FILTER:
+            return {...state, filters:payload};
+        default:
+            return state;
+    }
+}
+
+export default todoReducer;
