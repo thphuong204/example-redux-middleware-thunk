e22b5ebb5a9d5d7c7b4a6622bfa162b3354fe4c6
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-16 16:18:18 +0700

    create @TodoAdd component to dispatch of new object to store

---
+++ src/features/todo/TodoAdd.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { addTodo } from './action';
+
+function TodoAdd() {
+    const [text,setText] = useState("");
+    const dispatch = useDispatch();
+  return (
+    <div>
+        <input 
+        type="text" 
+        value={text}
+        onChange = {(e)=>setText(e.target.value)}
+        />
+        <button
+        onClick={()=>{
+            dispatch(addTodo(text));
+            setText("");
+        }}
+        >
+            Add</button>
+    </div>
+  )
+}
+
+export default TodoAdd
