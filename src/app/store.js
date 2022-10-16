27c21d11764b570514f23eb3a994624b48f62cda
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-16 16:15:41 +0700

    store todoReducer in @store.js

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,7 @@ import { createStore,combineReducers } from "redux";
 import counterReducer from "../features/counter/reducer";
 import {composeWithDevTools} from "redux-devtools-extension";
 import multiCounterReducer from "../features/multiCounter/reducer";
+import todoReducer from "../features/todo/reducer";
 
 
 const initialState = {}; //global state
@@ -9,6 +10,7 @@ const store = createStore(
     combineReducers({
         counter: counterReducer,
         multiCounter: multiCounterReducer,
+        todo: todoReducer,
     }),
     initialState,
     composeWithDevTools()
