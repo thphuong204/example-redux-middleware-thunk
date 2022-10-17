66ae7c53cd01129ba83e26cd712834d1e3f70ee8
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-17 08:54:36 +0700

    create TodoFilterButtons component to render 3 buttons for onClick => dispatch action setFilter to change filter in global state

---
+++ src/features/todo/TodoFilterButtons.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setFilter } from './action';
+
+function TodoFilterButtons() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button onClick={()=> dispatch(setFilter("SHOW_ALL"))}>Show All</button>
+      <button onClick={()=> dispatch(setFilter("SHOW_COMPLETED"))}>Show Completed</button>
+      <button onClick={()=> dispatch(setFilter("SHOW_ACTIVE"))}>Show Active</button>
+
+    </div>
+  )
+}
+
+export default TodoFilterButtons
