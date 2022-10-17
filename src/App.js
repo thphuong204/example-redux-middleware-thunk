b96a882d450f9f35151e019c76811a24dd5f06c8
thphuong204 <107916151+thphuong204@users.noreply.github.com>
2022-10-17 08:55:36 +0700

    render @Todo component in @App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Counter from "./features/counter/Counter";
 import MultiCounter from "./features/multiCounter/MultiCounter";
+import Todo from "./features/todo/Todo";
 
 
 function App() {
   return (
     <div className="App">
       <h1>Intro Redux</h1>
-      <MultiCounter/>
+      <Todo/>
     </div>
   );
 }
